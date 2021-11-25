8772d4aeeaaa47acf773e0e07e2907f1cacfb53f
er.ankit.cs <er.ankit.cs@gmail.com>
2021-11-26 00:22:28 +1100

    cdk workshop first commit.

--- awscdk_workshop/awscdk_workshop_stack.py
+++ awscdk_workshop/awscdk_workshop_stack.py
@@ -1,24 +1,17 @@
-from aws_cdk import (
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-    core
-)
 
+from aws_cdk import (
+    core,
+    aws_lambda as _lambda,
+)
 
 class AwscdkWorkshopStack(core.Stack):
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        queue = sqs.Queue(
-            self, "AwscdkWorkshopQueue",
-            visibility_timeout=core.Duration.seconds(300),
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime = _lambda.Runtime.PYTHON_3_7,
+            code  = _lambda.Code.from_asset('lambda'),
+            handler = 'hello.handler',
         )
 
-        topic = sns.Topic(
-            self, "AwscdkWorkshopTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
