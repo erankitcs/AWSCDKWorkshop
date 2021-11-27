b95f16969d3b9fad27434b386026e5b7d1be5b3d
er.ankit.cs <er.ankit.cs@gmail.com>
2021-11-27 21:36:59 +1100

    Pipeline commit.

--- awscdk_workshop/pipeline_stack.py
+++ awscdk_workshop/pipeline_stack.py
@@ -1,6 +1,9 @@
 from aws_cdk import (
     core,
     aws_codecommit as codecommit,
+    aws_codepipeline as codepipeline,
+    aws_codepipeline_actions as codepipeline_actions,
+    pipelines as pipelines
 )
 #from pipeline_stage import WorkshopPipelineStage
 
@@ -14,4 +17,29 @@ class WorkshopPipelineStack(core.Stack):
         repo = codecommit.Repository(
             self, 'WorkshopRepo',
             repository_name= "WorkshopRepo"
+        )
+
+        source_artifact = codepipeline.Artifact()
+        cloud_assembly_artifact = codepipeline.Artifact()
+
+        pipeline = pipelines.CdkPipeline(
+            self, 'CDKWorkshopPipeline',
+            cloud_assembly_artifact = cloud_assembly_artifact,
+
+            ### Generate source artifact from repository.
+            source_action = codepipeline_actions.CodeCommitSourceAction(
+                action_name = 'CodeCommit', ##  Any GIT bash source here.
+                output = source_artifact, ## where artifact is stored
+                repository = repo ## repo to draw the code.
+            ),
+            ## Build Source code into Cloud Assembly artifact.
+            synth_action = pipelines.SimpleSynthAction(
+                install_commands = [
+                    'npm install -g aws-cdk',
+                    'pip install -r requirements.txt'
+                ],
+                synth_command ='npx cdk synth',
+                source_artifact=source_artifact,
+                cloud_assembly_artifact= cloud_assembly_artifact
+            )
         )
